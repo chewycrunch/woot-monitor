ac2bc73799222a6aafa13c1737648ec2b8904b18
David Chew <36521499+chewycrunch@users.noreply.github.com>
2026-08-21 00:26:15 -0400

    drop glob reexports

--- monitor/src/monitor/mod.rs
+++ monitor/src/monitor/mod.rs
@@ -2,10 +2,6 @@ pub mod instance;
 pub mod notify;
 pub mod product;
 pub mod scrape;
+pub mod tls_api;
 pub mod transform;
 pub mod woot_api;
-pub mod tls_api;
-
-pub use product::*;
-pub use woot_api::*;
-pub use tls_api::*;
